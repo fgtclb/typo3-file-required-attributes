2fce36aa311a9fdae7058a82f386c7794a668cf7
Markus Hofmann <m.hofmann@web-vision.de>
2023-04-12 15:54:04 +0200

    [TASK] set correct labels, add not set radio item

--- Configuration/TCA/Overrides/sys_file_metadata.php
+++ Configuration/TCA/Overrides/sys_file_metadata.php
@@ -3,16 +3,20 @@
 (static function (): void {
     $newColumns = [
         'right_of_use' => [
-            'label' => 'LLL:EXT:file_required_attribute/Resources/Private/Language/locallang_be.xlf:sys_file_metadata.right_of_use',
+            'label' => 'LLL:EXT:file_required_attributes/Resources/Private/Language/locallang_be.xlf:sys_file_metadata.right_of_use',
             'config' => [
                 'type' => 'radio',
                 'items' => [
                     [
-                        'LLL:EXT:file_required_attribute/Resources/Private/Language/locallang_be.xlf:sys_file_metadata.right_of_use.full',
+                        'LLL:EXT:file_required_attributes/Resources/Private/Language/locallang_be.xlf:sys_file_metadata.right_of_use.notSet',
+                        0,
+                    ],
+                    [
+                        'LLL:EXT:file_required_attributes/Resources/Private/Language/locallang_be.xlf:sys_file_metadata.right_of_use.full',
                         1,
                     ],
                     [
-                        'LLL:EXT:file_required_attribute/Resources/Private/Language/locallang_be.xlf:sys_file_metadata.right_of_use.limited',
+                        'LLL:EXT:file_required_attributes/Resources/Private/Language/locallang_be.xlf:sys_file_metadata.right_of_use.limited',
                         2,
                     ],
                 ],
