e7470561d24b14f88c4ecb2e376dd23ca00be3e4
Markus Hofmann <m.hofmann@web-vision.de>
2023-04-12 10:48:38 +0200

    [TASK] Kickstart ext required_attributes

---
+++ ext_emconf.php
@@ -0,0 +1,24 @@
+<?php
+
+$EM_CONF[$_EXTKEY] = [
+    'title' => '(web-vision) Site Template',
+    'description' => 'Marks metadata fields required and disables fire references if required fields are missing',
+    'category' => 'fe,be',
+    'state' => 'beta',
+    'version' => '1.0.0',
+    'constraints' => [
+        'depends' => [
+            'typo3' => '11.5',
+        ],
+    ],
+    'autoload' => [
+        'psr-4' => [
+            'FGTCLB\\FileRequiredAttributes\\' => 'Classes/'
+        ],
+    ],
+    'autoload-dev' => [
+        'psr-4' => [
+            'FGTCLB\\FileRequiredAttributes\\Tests\\' => 'Tests/'
+        ],
+    ],
+];
