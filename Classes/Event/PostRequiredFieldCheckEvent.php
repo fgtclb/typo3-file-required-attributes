c59c145f06d349d621b750df385ab78b92e7c4c9
Mark <markhouben91@gmail.com>
2024-06-27 17:03:27 +0200

    [TASK] Add event dispatch to check require felds changes

---
+++ Classes/Event/PostRequiredFieldCheckEvent.php
@@ -0,0 +1,54 @@
+<?php
+
+declare(strict_types = 1);
+
+namespace FGTCLB\FileRequiredAttributes\Event;
+
+final class PostRequiredFieldCheckEvent
+{
+    /**
+     * @var array<string, mixed>
+     */
+    private array $data;
+
+    /**
+     * @var array<int, string>
+     */
+    private array $requiredColumns;
+
+    /**
+     * @param array<string, mixed> $data
+     * @param array<int, string> $requiredColumns
+     */
+    public function __construct(
+        array $data,
+        array $requiredColumns
+    ) {
+        $this->data = $data;
+        $this->requiredColumns = $requiredColumns;
+    }
+
+    /**
+     * @return array<string, mixed>
+     */
+    public function getData(): array
+    {
+        return $this->data;
+    }
+
+    /**
+     * @return array<int, string>
+     */
+    public function getRequiredColumns(): array
+    {
+        return $this->requiredColumns;
+    }
+
+    /**
+     * @param array<int, string> $requiredColumns
+     */
+    public function setRequiredColumns(array $requiredColumns): void
+    {
+        $this->requiredColumns = $requiredColumns;
+    }
+}
