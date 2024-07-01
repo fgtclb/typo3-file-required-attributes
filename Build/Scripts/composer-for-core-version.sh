228c7f177f089b7031426b6683eb868f8fcc6125
mhofmann <m.hofmann@web-vision.de>
2024-07-01 16:50:28 +0200

    [TASK] Introduce testing structure
    For further development and a better code structure, it is necessary to
    introduce a valid testing structure. Beside adding TYPO3 Testing
    Framework's used runTests script, some additional packages where
    introduced for running automated code styling and linting.
    
    Used commands:
    ```sh
    composer config sort-packages "true"
    composer req --dev typo3/testing-framework:^7.0
    composer req --dev friendsofphp/php-cs-fixer:^3.59
    composer req --dev phpstan/phpstan:^1.11
    composer req --dev phpunit/phpunit:^10.5
    composer req --dev saschaegerer/phpstan-typo3:^1.10
    composer req --dev staabm/phpstan-todo-by:^0.1.27
    composer req --dev helmich/typo3-typoscript-lint:^3.2
    composer req "typo3/cms-core:^11.5 || ^12.4"
    composer req "typo3/cms-backend:^11.5 || ^12.4"
    composer req "typo3/cms-filemetadata:^11.5 || ^12.4"
    ```

---
+++ Build/Scripts/composer-for-core-version.sh
@@ -0,0 +1,50 @@
+#!/usr/bin/env bash
+
+composer_cleanup() {
+    echo -e "💥 Cleanup folders"
+    rm -Rf \
+        .Build/vendor/* \
+        .Build/var/* \
+        .Build/bin/* \
+        .Build/Web/typo3conf/ext/* \
+        .Build/Web/typo3/* \
+        .Build/Web/typo3temp/* \
+        composer.lock
+}
+
+composer_update() {
+    echo -e "🔥 Update to selected dependencies"
+    php -dxdebug.mode=off $(which composer) install
+
+    echo -e "🌊 Restore composer.json"
+    git restore composer.json
+}
+
+update_v12() {
+    echo -e "💪 Enforce TYPO3 v12"
+    php -dxdebug.mode=off $(which composer) require --no-update \
+        "typo3/cms-core":"^12.4"
+}
+
+update_v11() {
+    echo -e "💪 Enforce TYPO3 v11"
+    php -dxdebug.mode=off $(which composer) require --no-update \
+        "typo3/cms-core":"^11.5"
+}
+
+case "$1" in
+12)
+    composer_cleanup
+    update_v12
+    composer_update
+    ;;
+11)
+    composer_cleanup
+    update_v11
+    composer_update
+    ;;
+*)
+    echo -e "🌀 Usage: ddev update-to (11|12)" >&2
+    exit 0
+    ;;
+esac
