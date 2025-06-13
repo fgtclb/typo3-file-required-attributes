1dfdc213a3e12f3e9324edf8d371d30ea2b9b81f
Markus Hofmann <m.hofmann@web-vision.de>
2025-06-13 14:34:34 +0200

    [TASK] Increase version number
    Release of version 2. Usable for TYPO3 12 and 13

--- ext_emconf.php
+++ ext_emconf.php
@@ -5,20 +5,13 @@ $EM_CONF[$_EXTKEY] = [
     'description' => 'Marks metadata fields required and disables file references if required fields are missing',
     'category' => 'fe,be',
     'state' => 'beta',
-    'version' => '1.1.1',
+    'version' => '2.0.0',
     'constraints' => [
         'depends' => [
             'typo3' => '12.4.0-13.4.99',
-        ],
-    ],
-    'autoload' => [
-        'psr-4' => [
-            'FGTCLB\\FileRequiredAttributes\\' => 'Classes/',
-        ],
-    ],
-    'autoload-dev' => [
-        'psr-4' => [
-            'FGTCLB\\FileRequiredAttributes\\Tests\\' => 'Tests/',
+            'backend' => '12.4.0-13.4.99',
+            'filelist' => '12.4.0-13.4.99',
+            'filemetadata' => '12.4.0-13.4.99',
         ],
     ],
 ];
